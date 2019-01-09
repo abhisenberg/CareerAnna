15d3c08144b966c5c494a83c3308faa67eb75b40
abhisenberg <ab96sh@gmail.com>
2019-01-09 17:05:28 +0530

    Improved margins of card layouts

--- app/src/main/java/com/careeranna/careeranna/activity/PurchaseCourseDetail.java
+++ app/src/main/java/com/careeranna/careeranna/activity/PurchaseCourseDetail.java
@@ -53,6 +53,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.longtailvideo.jwplayer.events.FullscreenEvent;
 import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents;
+import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -585,26 +586,26 @@ public class PurchaseCourseDetail extends AppCompatActivity implements VideoPlay
                 }
             }
 
-            if (!mUnits.isEmpty()) {
-                if (!mUnits.get(0).topics.isEmpty()) {
-                    if (!mUnits.get(0).topics.get(0).getVideos().equals("")) {
-                        playVideo(mUnits.get(0).topics.get(0).getVideos());
-                    }
-                }
-            }
+//            if (!mUnits.isEmpty()) {
+//                if (!mUnits.get(0).topics.isEmpty()) {
+//                    if (!mUnits.get(0).topics.get(0).getVideos().equals("")) {
+//                        playVideo(mUnits.get(0).topics.get(0).getVideos());
+//                    }
+//                }
+//            }
             listAdapter = new ExpandableListAdapterForNestedScroll(getApplicationContext(), mUnits, expandableListView);
             expandableListView.setAdapter(listAdapter);
         }
 
     }
 
-    private void playVideo(String videoUrl) {
-        PlaylistItem playlistItem = new PlaylistItem.Builder()
-                .file(videoUrl)
-                .build();
-        playerView.load(playlistItem);
-        playerView.play();
-    }
+//    private void playVideo(String videoUrl) {
+//        PlaylistItem playlistItem = new PlaylistItem.Builder()
+//                .file(videoUrl)
+//                .build();
+//        playerView.load(playlistItem);
+//        playerView.play();
+//    }
 
     public void paidCourseCheckout(final String price){
         Dialog dialog = new Dialog(this);
